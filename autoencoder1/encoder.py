25872471901d3321521d58fc0f2ddf8897af2b4a
ChadMcintire <chadmcintirecloud@gmail.com>
2020-02-27 13:41:43 -0700

    Fully functional encoder

---
+++ autoencoder1/encoder.py
@@ -0,0 +1,55 @@
+from keras.layers import Input, Conv2D, LeakyReLU, Flatten, Dense
+from keras.models import Model
+from keras import backend as K
+
+
+#function api for an encoder
+class Encoder():
+    def __init__(self,
+                 input_dim,
+                 encoder_conv_filters,
+                 encoder_conv_kernel_size,
+                 encoder_conv_strides,
+                 z_dim, 
+                 use_batch_norm,
+                 use_dropout
+                 ):
+        self.input_dim = input_dim    
+        self.name = 'decoder'
+        self.num_layers = len(encoder_conv_filters)
+        self.encoder_conv_filters = encoder_conv_filters
+        self.encoder_conv_kernel_size = encoder_conv_kernel_size
+        self.encoder_conv_strides = encoder_conv_strides
+        self.z_dim = z_dim
+        self.use_batch_norm = use_batch_norm
+        self.use_dropout = use_dropout
+        self.layer_setup()
+
+    def layer_setup(self):
+        encoder_input = Input(shape=self.input_dim, name='encoder_input')
+        x = encoder_input
+
+        for i in range(self.num_layers):
+            conv_layer = Conv2D(
+                filters = self.encoder_conv_filters[i]
+                , kernel_size = self.encoder_conv_kernel_size[i]
+                , strides = self.encoder_conv_strides[i]
+                , padding = 'same'
+                , name = 'encoder_conv_' + str(i)
+                )
+
+            x = conv_layer(x)
+    
+            x = LeakyReLU()(x)
+    
+            if self.use_batch_norm:
+                x = BatchNormalization()(x)
+    
+            if self.use_dropout:
+                x = Dropout(rate = 0.25)(x)
+
+        self.shape_before_flattening = K.int_shape(x)[1:]
+        x = Flatten()(x)
+        encoder_output= Dense(self.z_dim, name='encoder_output')(x)
+
+        self.encoder = Model(encoder_input, encoder_output)
